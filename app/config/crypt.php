bbfb132aabcbdddd9c7f356f8355a020223268bb
katokaisya <email>
2016-02-11 11:40:08 +0900

    Donichi-app First commit

---
+++ app/config/crypt.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+  'crypto_key' => 'fyM67ApXoYN4WroZZ4mFQ9do',
+  'crypto_iv' => 'zTQLTYirQh1Yw_YyaIYkYB24',
+  'crypto_hmac' => 'DgIKZ4RuUh_8SEsBjEH3gHcI',
+);
---
+++ app/logs/2016/02/10.php
@@ -0,0 +1,11 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2016-02-10 19:19:26 --> Parsing Error - syntax error, unexpected T_OBJECT_OPERATOR in /mnt/fuelphp/fuel/app/classes/controller/tweet.php on line 15
+ERROR - 2016-02-10 20:00:14 --> 23000 - SQLSTATE[23000]: Integrity constraint violation: 1048 Column 'ip' cannot be null with query: "INSERT INTO `requests` (`body`, `ip`, `created_at`, `updated_at`) VALUES ('会社のPCルームの蛍光灯が消えかかっています。取り換えてください。超怖いです。目にも悪いです。', null, 1455102014, 1455102014)" in /mnt/fuelphp/fuel/core/classes/database/pdo/connection.php on line 272
+ERROR - 2016-02-10 20:32:13 --> Parsing Error - syntax error, unexpected T_STRING in /mnt/fuelphp/fuel/app/views/request/index.php on line 20
+ERROR - 2016-02-10 20:34:55 --> Parsing Error - syntax error, unexpected T_STRING in /mnt/fuelphp/fuel/app/views/request/index.php on line 25
+ERROR - 2016-02-10 20:35:00 --> Parsing Error - syntax error, unexpected T_STRING in /mnt/fuelphp/fuel/app/views/request/index.php on line 25
+ERROR - 2016-02-10 20:36:40 --> Parsing Error - syntax error, unexpected T_STRING in /mnt/fuelphp/fuel/app/views/request/index.php on line 26
+ERROR - 2016-02-10 20:37:16 --> Parsing Error - syntax error, unexpected T_STRING in /mnt/fuelphp/fuel/app/views/request/index.php on line 26
+ERROR - 2016-02-10 20:39:01 --> Parsing Error - syntax error, unexpected T_STRING in /mnt/fuelphp/fuel/app/views/request/index.php on line 26
+ERROR - 2016-02-10 21:18:44 --> Runtime Recoverable error - Argument 3 passed to Fuel\Core\Form::label() must be an array, string given, called in /mnt/fuelphp/fuel/app/views/request/_form.php on line 5 and defined in /mnt/fuelphp/fuel/core/classes/form.php on line 280
